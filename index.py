141cc3025e19febe8eb6e0babafbce7876080c54
Yimura <andreas.maerten@scarlet.be>
2021-04-21 15:39:41 +0200

    wip(MazeSolver): Selecting start & end nodes

--- index.py
+++ index.py
@@ -1,9 +1,40 @@
 from maze import Maze
 from maze_gui import MazeGUI
-# from maze_solver import MazeSolver
+from maze_solver import MazeSolver
 
 maze = Maze(row_column_amount = 50)
-maze.generate()
-
 maze_gui = MazeGUI(maze = maze, pixel_size = 15)
+maze_solver = MazeSolver()
+
+def callback(e):
+    global maze, maze_gui, maze_solver
+
+    if not maze.is_finished or (maze_solver.start and maze_solver.end):
+        return
+
+    node = maze.nodes[e.x // maze_gui.pixel_size][e.y // maze_gui.pixel_size]
+    if node.is_wall:
+        return
+
+    if not maze_solver.start:
+        maze_solver.start = node
+
+        node.color = 'blue'
+        maze_gui.draw_node(node)
+
+        return
+
+    node.color = 'green'
+    maze_gui.draw_node(node)
+
+    maze_solver.end = node
+
+    # while maze_solver.step() == 0:
+    #     maze_solver.backtrack()
+
+    #     maze_gui.update()
+
+maze_gui.canvas.bind("<Button-1>", callback)
+
+maze.generate()
 maze_gui.draw_maze()
--- maze_solver.py
+++ maze_solver.py
@@ -1,3 +1,22 @@
+from numpy import sqrt
+
 class MazeSolver(object):
     def __init__(self):
-        pass
+        self.open_set = []
+        self.closed_set = {}
+
+        self._start = None
+        self.end = None
+
+    @property
+    def start(self):
+        return self._start
+
+    @start.setter
+    def start(self, value):
+        self.last_checked_node = value
+
+        self.current = value
+        self._start = value
+
+        self.open_set.append(value)
